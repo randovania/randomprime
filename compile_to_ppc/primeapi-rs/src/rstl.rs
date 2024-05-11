7d0e5694cf46027331b6836b1784d6e31303c447
Kevin Marsolais <kevin@kevinmarsolais.co>
2024-05-11 10:55:35 -0700

    `cargo fmt` in `compile_to_ppc` workspace

--- compile_to_ppc/primeapi-rs/src/rstl.rs
+++ compile_to_ppc/primeapi-rs/src/rstl.rs
@@ -1,17 +1,16 @@
 use core::mem::MaybeUninit;
+
 use crate::cw_link_name;
 
 #[repr(C)]
-struct WStringCowData
-{
+struct WStringCowData {
     capacity: u32,
     ref_count: u32,
     // data: [u16],
 }
 
 #[repr(C)]
-pub struct WString
-{
+pub struct WString {
     data: *mut u16,
     cow: *mut WStringCowData,
     size: usize,
@@ -31,19 +30,16 @@ extern "C" {
     fn ctor_reserve(this: *mut WString, cap: u32);
 }
 
-impl WString
-{
-    pub fn from_ucs2_str(s: *const u16) -> Self
-    {
-        let mut this = MaybeUninit::uninit();// WString(MaybeUninit::uninit());
+impl WString {
+    pub fn from_ucs2_str(s: *const u16) -> Self {
+        let mut this = MaybeUninit::uninit(); // WString(MaybeUninit::uninit());
         unsafe {
             wstr_ctor(this.as_mut_ptr(), s);
             this.assume_init()
         }
     }
 
-    pub fn with_capacity(cap: usize) -> Self
-    {
+    pub fn with_capacity(cap: usize) -> Self {
         let mut this = MaybeUninit::uninit();
         let mut this = unsafe {
             ctor_reserve(this.as_mut_ptr(), cap as u32 + 1);
@@ -53,8 +49,7 @@ impl WString
         this
     }
 
-    pub fn from_ascii(s: &[u8]) -> Self
-    {
+    pub fn from_ascii(s: &[u8]) -> Self {
         let mut this = Self::with_capacity(s.len());
         for (i, b) in s.iter().enumerate() {
             unsafe {
@@ -69,20 +64,16 @@ impl WString
     }
 }
 
-impl Drop for WString
-{
-    fn drop(&mut self)
-    {
+impl Drop for WString {
+    fn drop(&mut self) {
         unsafe {
             dtor(self);
         }
     }
 }
 
-
 #[repr(C)]
-pub struct Vector<T>
-{
+pub struct Vector<T> {
     // XXX Is _first actually used? primeapi doesn't seem to think so
     _first: usize,
     size: usize,
@@ -90,58 +81,37 @@ pub struct Vector<T>
     data: *mut T,
 }
 
-impl<T> Vector<T>
-{
-    pub fn size(&self) -> usize
-    {
+impl<T> Vector<T> {
+    pub fn size(&self) -> usize {
         self.size
     }
 
-    pub fn capacity(&self) -> usize
-    {
+    pub fn capacity(&self) -> usize {
         self.capacity
     }
 }
 
-impl<T> core::ops::Deref for Vector<T>
-{
+impl<T> core::ops::Deref for Vector<T> {
     type Target = [T];
-    fn deref(&self) -> &Self::Target
-    {
-        unsafe {
-            core::slice::from_raw_parts(self.data, self.size)
-        }
+    fn deref(&self) -> &Self::Target {
+        unsafe { core::slice::from_raw_parts(self.data, self.size) }
     }
 }
 
-impl<T> core::ops::DerefMut for Vector<T>
-{
-    fn deref_mut(&mut self) -> &mut Self::Target
-    {
-        unsafe {
-            core::slice::from_raw_parts_mut(self.data, self.size)
-        }
+impl<T> core::ops::DerefMut for Vector<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { core::slice::from_raw_parts_mut(self.data, self.size) }
     }
 }
 
-impl<T> Into<alloc::vec::Vec<T>> for Vector<T>
-{
-    fn into(self) -> alloc::vec::Vec<T>
-    {
-        unsafe {
-            alloc::vec::Vec::from_raw_parts(
-                self.data,
-                self.size,
-                self.capacity,
-            )
-        }
+impl<T> Into<alloc::vec::Vec<T>> for Vector<T> {
+    fn into(self) -> alloc::vec::Vec<T> {
+        unsafe { alloc::vec::Vec::from_raw_parts(self.data, self.size, self.capacity) }
     }
 }
 
-impl<T> From<alloc::vec::Vec<T>> for Vector<T>
-{
-    fn from(vec: alloc::vec::Vec<T>) -> Self
-    {
+impl<T> From<alloc::vec::Vec<T>> for Vector<T> {
+    fn from(vec: alloc::vec::Vec<T>) -> Self {
         Vector {
             _first: 0,
             size: vec.len(),
